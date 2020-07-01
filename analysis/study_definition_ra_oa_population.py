8e0ecd9a45ed9dabf97c0ce4bf81e555135e82ee
Brian <brianmkcebm@gmail.com>
2020-07-01 06:57:54 +0100

    tweak count
    test failing as not returning integer. removing the expectataions to see what happens

--- analysis/study_definition_ra_oa_population.py
+++ analysis/study_definition_ra_oa_population.py
@@ -673,11 +673,8 @@ study = StudyDefinition(
 
     ##A&E ATTENDANCE IN PREVIOUS YEAR
     annde_attendance_last_year=patients.attended_emergency_care(
-    between=["2019-03-01", "2020-02-29"],
-    returning="number_of_matches_in_period",
-    return_expectations={"date": {"earliest": "2019-03-01"}},
-     
-    ),
+    between=["2019-03-01", "2020-02-29"], returning="number_of_matches_in_period",
+     ),
 
     ### GP CONSULTATION RATE
     gp_consult_count=patients.with_gp_consultations(
