57c065f4395447e7e19e35b90f33ef522228eba8
Brian <brianmkcebm@gmail.com>
2020-07-01 07:12:29 +0100

    return day of attendance

--- analysis/study_definition_nsaid_population.py
+++ analysis/study_definition_nsaid_population.py
@@ -61,6 +61,7 @@ study = StudyDefinition(
     aande_attendance_with_covid=patients.attended_emergency_care(
         on_or_after="2020-03-01",
         returning="date_arrived",
+        date_format="YYYY-MM-DD",
         with_these_diagnoses=ics_codes, #placeholder https://github.com/opensafely/cohort-extractor/issues/182#issuecomment-651782064
         return_expectations={"date": {"earliest": "2020-03-01"}},
     ),
