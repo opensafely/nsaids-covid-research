de72f6281238ff683c34b15be9993d0640eb3b3d
angelwong121 <63405380+angelwong121@users.noreply.github.com>
2020-10-05 15:53:10 +0100

    Update study_definition_nsaid_population.py
    add one variable to identify people from the non-user group but who had an NSAID prescription in the last 2 years? Or last 12 months. The latest prescription date can help check whether they have a prescription in the last 2 years/last 12 months. This is a post-hoc anaylsis for paper revision

--- analysis/study_definition_nsaid_population.py
+++ analysis/study_definition_nsaid_population.py
@@ -117,9 +117,22 @@ study = StudyDefinition(
         include_month=True,
         include_day=False,
         return_expectations={
-            "date": {"earliest": "2020-01-01", "latest": "2020-02-29"}
+            "date": {"earliest": "2020-02-01", "latest": "2020-02-29"}
         },
     ),
+
+    recent_stop_nsaid=patients.with_these_medications(
+        nsaid_codes,
+        between=["2018-02-28", "2019-10-31"],
+        returning="date",
+        find_last_match_in_period=True,
+        include_month=True,
+        include_day=False,
+        return_expectations={
+            "date": {"earliest": "2018-02-28", "latest": "2019-10-31"}
+        },
+    ),
+
     # naproxen - high dose
     naproxen_high=patients.with_these_medications(
         naproxen_high_codes,
